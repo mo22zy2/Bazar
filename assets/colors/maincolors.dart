e0090f1e94069ea6bfdd95a75363efe54a815327
mohamed <moja3222@gmail.com>
2026-03-05 21:29:04 +0200

    add project files

---
+++ assets/colors/maincolors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MainColors {
+  static Color mainPurple = Color(0xff54408C);
+  static Color mainWhite = Color(0xffFAFAFA);
+  static Color mainGrey = Color(0xffF5F5F5);
+  static Color mainDarkGrey = Color(0xffD6D6D6);
+}
