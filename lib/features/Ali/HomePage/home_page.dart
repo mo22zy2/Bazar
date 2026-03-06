4eef5431ef2a7c8e2192455dedd786281788227b
Ali Mohamed <186Alo.hag@gmail.com>
2026-03-06 03:25:19 +0200

    Add home_page

---
+++ lib/features/Ali/HomePage/home_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+// ignore: camel_case_types
+class Home_page extends StatelessWidget {
+  const Home_page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Text('home Page'));
+  }
+}
