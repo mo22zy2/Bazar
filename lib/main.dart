7fd6d6615564a5e08a55602a96b35e6d21ab85f3
mohamed <moja3222@gmail.com>
2026-03-08 21:37:15 +0200

    Main entry point of main

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:bazar/features/Atef/Splach_screen.dart';
+import 'package:bazar/firebase_options.dart';
 import 'package:flutter/material.dart';
+
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp();
-
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
 
