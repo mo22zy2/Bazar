ed20b9a14d606ade4a53ec709fb87b4ab00aff91
mohamed <moja3222@gmail.com>
2026-03-08 21:27:43 +0200

    adding firebase Auth for sign in and signup

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,11 @@
 import 'package:bazar/features/Atef/Splach_screen.dart';
-import 'package:bazar/firebase_options.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
 
   runApp(const MainApp());
 }
