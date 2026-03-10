550dc67452cbd7201eefbcecd9fd77f4fc6673e3
mohamed <moja3222@gmail.com>
2026-03-10 21:37:57 +0200

    Main splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:bazar/features/Atef/Splach_screen.dart';
+import 'package:bazar/features/islam/Confirm_Order/Confirm_Order_visaAdded.dart';
+import 'package:bazar/features/islam/My_Cart/my_cart.dart';
 import 'package:bazar/firebase_options.dart';
 import 'package:flutter/material.dart';
 
